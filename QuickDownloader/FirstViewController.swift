e46a487ff81192647ff5b1b44474dbdbccb50ed3
Zaiyang Li <zaiyangli777@gmail.com>
2016-07-22 00:44:05 +0100

    Initial Commit

---
+++ QuickDownloader/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  QuickDownloader
+//
+//  Created by Zaiyang Li on 22/07/2016.
+//  Copyright © 2016 Zaiyang Li. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
